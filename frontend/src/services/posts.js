880891ab5e24bac46a99f0c8b78802d1deb8add7
Lucas Oliveira <lukorei@gmail.com>
2020-11-18 02:23:30 -0300

    Buscando posts no perfil do pet

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -1,39 +1,57 @@
 import { axiosInstance as axios } from 'src/boot/axios'
 
-function get (filtros) {
-  return axios
-    .get('/posts', { params: filtros })
-    .catch((error) => Promise.reject(error))
+async function get (filtros) {
+  try {
+    return axios
+      .get('/posts', { params: filtros })
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
-function like (idPost) {
-  return axios
-    .post(`/posts/${idPost}/like`)
-    .catch((error) => Promise.reject(error))
+async function like (idPost) {
+  try {
+    return axios
+      .post(`/posts/${idPost}/like`)
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
-function dislike (idPost) {
-  return axios
-    .post(`/posts/${idPost}/dislike`)
-    .catch((error) => Promise.reject(error))
+async function dislike (idPost) {
+  try {
+    return axios
+      .post(`/posts/${idPost}/dislike`)
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
-function publicar (body) {
-  return axios
-    .post('/posts', body)
-    .catch((error) => Promise.reject(error))
+async function publicar (body) {
+  try {
+    return axios
+      .post('/posts', body)
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
-function atualizar (idPost, body) {
-  return axios
-    .put('/posts/' + idPost, body)
-    .catch((error) => Promise.reject(error))
+async function atualizar (idPost, body) {
+  try {
+    return axios
+      .put('/posts/' + idPost, body)
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
-function apagar (idPost) {
-  return axios
-    .delete('/posts/' + idPost)
-    .catch((error) => Promise.reject(error))
+async function apagar (idPost) {
+  try {
+    return axios
+      .delete('/posts/' + idPost)
+  } catch (error) {
+    return await Promise.reject(error)
+  }
 }
 
 export const PostService = {
